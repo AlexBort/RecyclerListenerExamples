8eba382323fdfe8c2f745d4ae5ae7c45dab32beb
Oleksandr Bortnyk <oleksandr.bortnyk@vimmi.net>
2020-08-23 21:12:28 +0300

    first var of making listener (made custom listener which overrides TouchListener)

---
+++ app/src/main/java/com/example/recyclerlistenerexamples/RecyclerItemClickListener.kt
@@ -0,0 +1,43 @@
+package com.example.recyclerlistenerexamples
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerItemClickListener(context: Context?, listener: ItemClickListener?) : RecyclerView.OnItemTouchListener {
+
+    private var gestureDetector: GestureDetector
+    private var clickListener: ItemClickListener
+
+    init {
+        this.clickListener = listener!!
+        gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                return true
+            }
+        })
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        val childView: View? = rv.findChildViewUnder(e.x, e.y)
+        if (childView != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+            clickListener.onItemClick(childView, rv.getChildAdapterPosition(childView))
+            return true
+        }
+        return false
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+    }
+
+
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+}
